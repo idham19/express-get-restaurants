c2fcf7fbeb5a040d340df3edc46b98c36e5832e3
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-23 16:11:06 -0400

    put method to update a restaurant added to app.js

--- src/app.js
+++ src/app.js
@@ -32,4 +32,21 @@ app.post("/restaurant", async (req, res) => {
     res.status(500).send({ error: "Internal server error" });
   }
 });
+
+//update Method
+app.put("/restaurants/:id", async (req, res) => {
+  try {
+    const updatedRestaurantData = req.body;
+    const restaurantId = req.params.id;
+    const restaurantToUpdate = await Restaurant.findByPk(restaurantId);
+
+    await restaurantToUpdate.update(updatedRestaurantData);
+
+    const allRestaurants = await Restaurant.findAll();
+    res.status(200).json(allRestaurants);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Inter server error" });
+  }
+});
 module.exports = app;
