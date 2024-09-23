d6fca65da0075a49b84254f1c378e5ba1dfdaa18
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-23 12:48:28 -0400

    Get/restaurants/:id request added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
-app.get("/restaurants",async(request,response)=>{
+app.get("/restaurants", async (request, response) => {
   const findAllRestaurants = await Restaurant.findAll();
- return response.json(findAllRestaurants);
-
-})
+  return response.json(findAllRestaurants);
+});
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurantId = req.params.id;
+  const getRestaurantById = await Restaurant.findByPk(restaurantId);
+  if (getRestaurantById) {
+    res.json(getRestaurantById);
+  } else {
+    res.status(404).send({ message: "Restaurant not found" });
+  }
+});
 module.exports = app;
